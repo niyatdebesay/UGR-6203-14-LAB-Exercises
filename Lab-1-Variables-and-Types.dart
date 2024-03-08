84a4f8c8d7ed936d233bbf046039bdc4fe3dea94
Niyat Debesay <niyatdebesay26@gmail.com>
2024-03-08 15:10:43 +0300

    Lab-1-Variables-and-Types

---
+++ Lab-1-Variables-and-Types.dart
@@ -0,0 +1,11 @@
+void main(){
+var name = "Niyat Debesay";
+var age = 20;
+var FavColor = "Purple";
+
+print(" Hi My name is ${name} I am ${age} years old and my favourite color is ${FavColor}");
+
+
+
+
+}
